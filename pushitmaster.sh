123889d428322feaae251ff2d166d1b47f5ada5d
jeremykenedy <jeremykenedy@gmail.com>
2015-06-24 00:59:16 -0700

    ohhhh baby babyaay, o baby baby

---
+++ pushitmaster.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+## ADD VARIABLES HERE ##
+_yourSoundFileAndPath="/Users/jeremykenedy/Desktop/pushit-jk-master/pushit8s.m4a"
+
+## ADD ALIAS'S HERE ##
+alias pushitmaster="git push origin master;afplay "$_yourSoundFileAndPath
